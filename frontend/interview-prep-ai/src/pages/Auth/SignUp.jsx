e0ac6e6d9990c10a1148176e74a221133e0a12a6
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2025-07-03 22:59:37 +0530

    Add Sign Up

--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -1,8 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+import { validateEmail } from '../../utils/helper';
 
-const SignUp = () => {
+const SignUp = ({setCurrentPage}) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Handle SignUp form Submit
+  const handleSignUp = async (e) =>{
+    e.preventDefault();
+
+    let profileImageUrl ="";
+
+    if (!fullName) {
+      setError("Please enter full name.")
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a vald email address.")
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the passeord.")
+      return;
+    }
+
+    setError("");
+
+    // Sign Up API call 
+    try {
+      
+    } catch (error) {
+      setError(error.message || 'Something went wrong. Please try again.');
+    }
+
+
+  };
+
   return (
-    <div>SignUp</div>
+    <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
+      <h3 className='text-lg font-semibold text-black'>Create an Account</h3>
+      <p className='text-xs text-slate-700 mt-[5px] mb-6'>Join us today by entering your details below.</p>
+
+      <form onSubmit={handleSignUp}>
+
+        <ProfilePhotoSelector image={profilePic} setImage= {setProfilePic}/>
+
+        <div className='grid gridcols-1 md:grid-cols-1 gap-2'>
+          <Input 
+            value={fullName}
+            onChange={({target}) => setFullName(target.value)}
+            label="Full Name"
+            placeholder="vaibhav kulkarni"
+            type="text"
+          />
+
+          <Input
+            value={email}
+            onChange={({target}) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="vaibhav@example.com"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            onChange={({target}) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="password"
+          />
+
+        </div>
+        {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+        <button type='submit' className='btn-primary'>SIGN UP</button>
+
+        <p className='text-[13px] text-slate-800 mt-3'>
+          Already have an account?{" "}
+          <button
+            className='font-medium text-primary underline cursor-pointer'
+            onClick={() => {
+              setCurrentPage("login")
+            }}
+            >
+              Login
+            </button>
+        </p>
+      </form>
+    </div>
   )
 }
 
