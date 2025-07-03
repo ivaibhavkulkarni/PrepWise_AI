f6f6927705ff67b54918e281491679733ebaf200
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2025-07-03 22:59:19 +0530

    Add Login

--- frontend/interview-prep-ai/src/pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/Login.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
 
-const Login = () => {
+const validateEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
+
+const Login = ({ setCurrentPage }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address.');
+      return;
+    }
+
+    if (!password) {
+      setError('Please enter the password.');
+      return;
+    }
+
+    setError('');
+
+
+    // Login API call
+    try {
+      
+    } catch (error) {
+      setError(error.message || 'Something went wrong. Please try again.');
+    }
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
+      <h3 className='text-lg font-semibold text-black'>Welcome Back</h3>
+      <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+        Please enter your details to log in
+      </p>
 
-export default Login
+      <form onSubmit={handleLogin}>
+        <Input
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          label="Email Address"
+          placeholder="vaibhav@example.com"
+          type="text"
+        />
+
+        <Input
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          label="Password"
+          placeholder="Min 8 Characters"
+          type="password"
+        />
+
+        {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+        <button type='submit' className='btn-primary mt-3'>LOGIN</button>
+
+        <div className='text-[13px] text-slate-800 mt-3 flex gap-1 items-center'>
+          <span>Don't have an account?</span>
+          <button
+            type='button'
+            className='font-medium text-primary underline cursor-pointer'
+            onClick={() => setCurrentPage('signup')}
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
