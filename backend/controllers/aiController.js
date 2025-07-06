ec911f7e52f8eaa301aa627e52e4ac31eac0be30
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2025-07-06 21:52:02 +0530

    Add Gemini API, and testing

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,5 +1,5 @@
 const { GoogleGenAI } = require("@google/genai");
-const { conceptExplainPrompt } = require("../utils/prompts");
+const { conceptExplainPrompt, questionAnswerPrompt } = require("../utils/prompts");
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY});
 
@@ -9,7 +9,30 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY});
 
 const generateInterviewQuestions = async (req, res) => {
     try{
+        const { role, experience, topicsToFocus, numberOfQuestions} = req.body
 
+        if (!role || !experience || !topicsToFocus || !numberOfQuestions) {
+            return res.status(400).json({ message: "Missing required fields"});
+        }
+
+        const prompt = questionAnswerPrompt(role, experience, topicsToFocus, numberOfQuestions);
+
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash-lite",
+            contents: prompt,
+        });
+
+        let rawText = response.text;
+
+        // Clean it: Remove ```Json and ``` beginning and end
+        const cleanedText = rawText
+            .replace(/^```json\s*/,"") // remove starting ``` json
+            .replace(/```$/,"") // remove ending ```
+            .trim(); // remove extra spaces
+
+        // Now safe to parse 
+        const data = JSON.parse(cleanedText);
+        res.status(200).json(data);
     }catch(error){
         res.status(500).json({
             message: "Failed to generate questions",
