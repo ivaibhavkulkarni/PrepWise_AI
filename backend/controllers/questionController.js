59110dcc342dad84aa23ee3bb3766704979ffd6a
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2025-07-06 16:07:58 +0530

    Add pin question API

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -46,7 +46,18 @@ exports.addQuestionsToSession = async (req,res) => {
 
 exports.togglePinQuestion = async (req,res) => {
     try{
+        const question = await Question.findById(req.params.id);
 
+        if(!question) {
+            return res
+                .status(404)
+                .json({ success: false, message: "Question not found"});
+        }
+
+        question.isPinned = !question.isPinned;
+        await question.save();
+
+        res.status(200).json({ success: true, question});
     }catch(error){
         res.status(500).json({ message: "Server Error"})
     }
